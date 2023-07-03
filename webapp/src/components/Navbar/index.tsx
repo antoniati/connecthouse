26cc0f84fdae9ff1cc3bd1b48fe5ef5db496044a
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-07-03 18:45:56 -0300

    adjust for develop branch

--- webapp/src/components/Navbar/index.tsx
+++ webapp/src/components/Navbar/index.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import LogoImage from "../../assets/logo.svg";
 
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
     const [menuToggle, setMenuToggle] = useState("menu-toggle")
@@ -28,69 +29,89 @@ const Navbar: React.FC = () => {
     return (
         <>
             <nav id="navbar">
-                <img className="logo" alt="Logo Connect House" src={LogoImage} />
+                <img
+                    className="logo"
+                    alt="Logo Connect House"
+                    src={LogoImage}
+                />
 
                 <ul className="main-nav-list">
-                    <li className="main-nav-item">
-                            <a href="#about-section" className="main-nav-link">Sobre</a>
-                    </li>
-                    <li className="main-nav-item">
-                            <a href="#services-section" className="main-nav-link">Serviços</a>
-                    </li>
-                    <li className="main-nav-item">
-                            <a href="#projects-section" className="main-nav-link">Projetos</a>
-                    </li>
-                    <li className="main-nav-item">
-                            <a href="#contact-section" className="main-nav-link">Contato</a>
-                    </li>
+                    <Link to="https://connecthouse.vercel.app/sobre">
+                        <li className="main-nav-item">
+                            <a className="main-nav-link">Sobre</a>
+                        </li>
+                    </Link>
+                    <Link to="https://connecthouse.vercel.app/servicos">
+                        <li className="main-nav-item">
+                            <a className="main-nav-link">Serviços</a>
+                        </li>
+                    </Link>
+                    <Link to="https://connecthouse.vercel.app/projetos">
+                        <li className="main-nav-item">
+                            <a className="main-nav-link">Projetos</a>
+                        </li>
+                    </Link>
+                    <Link to="https://connecthouse.vercel.app/contato">
+                        <li className="main-nav-item">
+                            <a className="main-nav-link">Contato</a>
+                        </li>
+                    </Link>
                 </ul>
-
-                <div className={menuToggle} onClick={openMenuToggle}> <span></span> <span></span> <span></span> </div>
+                <div className={menuToggle} onClick={openMenuToggle} >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </nav>
 
             {/* DROP DOWN MENU */}
             <nav id={dropDown}>
                 <ul className="drop-down-nav-list">
-                    <li className="drop-down-nav-item">
-                            <a 
-                                href="#about-section" 
-                                className="drop-down-nav-link" 
+                    <Link to="https://connecthouse.vercel.app/sobre">
+                        <li className="drop-down-nav-item">
+                            <a
+                                className="drop-down-nav-link"
                                 data-text="Sobre"
-                                onClick={openMenuToggle} 
-                            > 
-                                    Sobre 
+                                onClick={openMenuToggle}
+                            >
+                                Sobre
                             </a>
-                    </li>
-                    <li className="drop-down-nav-item">
-                            <a 
-                                href="#services-section" 
-                                className="drop-down-nav-link" 
-                                data-text="Serviços" 
-                                onClick={openMenuToggle} 
-                            > 
-                                Serviços 
+                        </li>
+                    </Link>
+                    <Link to="https://connecthouse.vercel.app/servicos">
+                        <li className="drop-down-nav-item">
+                            <a
+                                className="drop-down-nav-link"
+                                data-text="Serviços"
+                                onClick={openMenuToggle}
+                            >
+                                Serviços
                             </a>
-                    </li>
-                    <li className="drop-down-nav-item">
-                            <a 
-                                href="#projects-section" 
-                                className="drop-down-nav-link" 
-                                data-text="Projetos"  
-                                onClick={openMenuToggle} 
-                            > 
-                                Projetos 
+                        </li>
+                    </Link>
+                    <Link to="https://connecthouse.vercel.app/projetos">
+                        <li className="drop-down-nav-item">
+                            <a
+                                className="drop-down-nav-link"
+                                data-text="Projetos"
+                                onClick={openMenuToggle}
+                            >
+                                Projetos
                             </a>
-                    </li>
-                    <li className="drop-down-nav-item">
-                            <a 
-                                href="#contact-section" 
-                                className="drop-down-nav-link" 
-                                data-text="Contato" 
-                                onClick={openMenuToggle} 
-                            > 
-                                Contato 
+                        </li>
+                    </Link>
+                    <Link to="https://connecthouse.vercel.app/contato">
+
+                        <li className="drop-down-nav-item">
+                            <a
+                                className="drop-down-nav-link"
+                                data-text="Contato"
+                                onClick={openMenuToggle}
+                            >
+                                Contato
                             </a>
-                    </li>
+                        </li>
+                    </Link>
                 </ul>
             </nav>
         </>
