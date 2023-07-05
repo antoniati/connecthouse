94d05f2ec48a13c073d534e50af2eb7e9f1e82c9
Felipe Antoniati <antoniati.felipe@gmail.com>
2023-07-04 21:48:09 -0300

    adjust for develop branch

--- webapp/src/pages/Projects/LigthingProjetct/index.tsx
+++ webapp/src/pages/Projects/LigthingProjetct/index.tsx
@@ -30,30 +30,30 @@ const LightingProject: React.FC = () => {
             <SectionContainer id="project-page">
                 <div className="project-content">
                     <div className="project-content-image_top">
-                    <img src="https://connecthouse.vercel.app/static/images/services/ilum/ilum-image-four.png" alt="" />
+                        <img src="https://connecthouse.vercel.app/static/images/services/ilum/ilum-six.png" alt="" />
                     </div>
 
                     <div className="project-content-introdution_item">
                         <h2 className="project-content-introdution_title">
-                            Instalação de Sistemas de CFTV
+                            Instalação Elétrica & Iluminação
                         </h2>
                         <p className="project-content-introdution_description">
-                            Nós, na <b>Connect House</b>, somos líderes em fornecer soluções de segurança de última geração para empresas e residências. Temos o prazer de apresentar um projeto recente em que realizamos a instalação de um sistema de CFTV para um cliente corporativo de renome. Neste caso de sucesso, fornecemos uma solução abrangente que atendeu às necessidades exclusivas do cliente, resultando em uma segurança aprimorada e tranquilidade.
+                            Realizamos recentemente um projeto de instalação elétrica e iluminação no Enterprise Station, em Campinas-SP. Com a nossa expertise e comprometimento, entregamos um serviço de qualidade que transformou completamente o ambiente. Durante a execução do projeto, utilizamos as mais avançadas técnicas e materiais de ponta para garantir uma instalação elétrica segura e eficiente.
                         </p>
                     </div>
 
                     <div className="project-content-details_item">
                         <div className="project-content-details_texts">
                             <h3 className="project-content-details_title">Local :</h3>
-                            <p className="project-content-details_description">Royal Plaza IN, Campinas-SP</p>
+                            <p className="project-content-details_description">Enterprise Station, Campinas-SP</p>
                         </div>
                         <div className="project-content-details_texts">
                             <h3 className="project-content-details_title">Data :</h3>
-                            <p className="project-content-details_description">01/04/2023</p>
+                            <p className="project-content-details_description">05/07/2023</p>
                         </div>
                         <div className="project-content-details_texts">
                             <h3 className="project-content-details_title">Tempo de execução :</h3>
-                            <p className="project-content-details_description">3 Meses</p>
+                            <p className="project-content-details_description">4 Meses</p>
                         </div>
                     </div>
 
@@ -61,19 +61,16 @@ const LightingProject: React.FC = () => {
                         <div className="project-content-analytics_texts">
                             <h2 className="project-content-analytics_title"> Análise Estratégica</h2>
                             <p className="project-content-analytics_description">
-                                Antes de iniciar a instalação do sistema de CFTV, nossa equipe de especialistas conduziu uma análise minuciosa das instalações do cliente.
-                            </p>
-                            <p className="project-content-analytics_description">
-                                Com base nessa avaliação criteriosa, identificamos os pontos críticos que requeriam monitoramento constante e desenvolvemos uma estratégia personalizada para maximizar a segurança.
+                                Durante a análise estratégica, identificamos as melhores soluções de iluminação para cada espaço. Utilizamos técnicas de iluminação focal e geral para criar um ambiente equilibrado e aconchegante. Além disso, incorporamos tecnologias avançadas, como sensores de presença e controle de iluminação automatizado, para otimizar o consumo de energia.
                             </p>
                         </div>
                         <div className="project-content-analytics_images">
                             <img
-                                src="https://connecthouse.vercel.app/static/images/services/cftv/cftv-image-four.png"
+                                src="https://connecthouse.vercel.app/static/images/services/external-work.png"
                                 alt="Imagem da preparação de cabos para instalação das camêras"
                             />
                             <img
-                                src="https://connecthouse.vercel.app/static/images/services/cftv/cftv-image-six.png"
+                                src="https://connecthouse.vercel.app/static/images/services/ilum/ilum-image-six.png"
                                 alt="Imagem da escada posisionada para instalão da caixa suspensa do servidor das camêras"
                             />
                         </div>
@@ -85,7 +82,11 @@ const LightingProject: React.FC = () => {
                                 Solução Personalizada
                             </h2>
                             <p className="project-content-solutions_description">
-                                Nossa equipe projetou um sistema de CFTV sob medida, levando em consideração os requisitos específicos do cliente. Utilizando equipamentos de ponta e câmeras de alta resolução, montamos um conjunto de câmeras estrategicamente posicionadas em locais-chave, como entradas, corredores e áreas com alto fluxo de pessoas. Combinando câmeras fixas e PTZ, garantimos uma cobertura abrangente e flexível.
+                                Nossa solução personalizada de iluminação foi cuidadosamente projetada para atender às demandas e expectativas do cliente. Selecionamos luminárias modernas e elegantes que complementam o design do ambiente, criando uma atmosfera sofisticada.
+                            </p>
+
+                            <p className="project-content-solutions_description">
+                                Utilizamos luminárias com diferentes intensidades de luz e tonalidades para criar diferentes cenários e realçar os pontos-chave do espaço. Além disso, aplicamos técnicas de iluminação arquitetural para destacar elementos específicos da decoração e criar um ambiente visualmente atraente.
                             </p>
                         </div>
                         <div className="project-content-solutions_images">
@@ -113,25 +114,26 @@ const LightingProject: React.FC = () => {
 
                     <div className="project-content-conclusion_item">
                         <h2 className="project-content-conclusion_title">
-                            Integração de Tecnologia Avançada
+                            Luminárias
                         </h2>
                         <p className="project-content-conclusion_description">
-                            Além da instalação das câmeras, integramos o sistema a um servidor de armazenamento em nuvem. Isso permite ao cliente acessar facilmente as gravações e imagens em tempo real, mesmo quando estão fora das instalações. A integração de tecnologia avançada garante a conveniência e a segurança dos dados.
+                            Nossas luminárias são escolhidas levando em consideração o estilo do local e as preferências do cliente. Desde lustres imponentes até spots de teto discretos, selecionamos as luminárias ideais para cada espaço, garantindo uma iluminação harmoniosa e agradável.
                         </p>
                         <h2 className="project-content-conclusion_title">
-                            Instalação Profissional e Treinamento
+                            Som Ambiente
                         </h2>
                         <p className="project-content-conclusion_description">
-                            Nossa equipe altamente qualificada e experiente realizou a instalação com habilidade e precisão, minimizando o impacto nas operações diárias do cliente. Além disso, oferecemos um treinamento completo à equipe interna do cliente, capacitando-os a utilizar todos os recursos do sistema de forma eficaz.
+                            Utilizamos equipamentos de áudio de alta qualidade para garantir uma reprodução de som cristalina. O sistema de som ambiente oferece a possibilidade de selecionar músicas de diferentes gêneros e ajustar o volume de acordo com a preferência de cada ambiente.
                         </p>
                         <h2 className="project-content-conclusion_title">
-                            Resultados e Benefícios
+                            Automação e instalação de TVs
                         </h2>
                         <p className="project-content-conclusion_description">
-                            O sistema de CFTV instalado oferece ao cliente uma visão abrangente de suas instalações, garantindo a prevenção de incidentes, monitoramento da segurança dos colaboradores e proteção de ativos valiosos. A solução personalizada proporciona tranquilidade e confiança ao cliente, tornando suas operações mais seguras e eficientes.
+                            No projeto, também incluímos automação residencial e a instalação de TVs. Através da automação, o cliente pode controlar a iluminação, o som ambiente e outros dispositivos eletrônicos de forma prática e intuitiva.
                         </p>
                         <p className="project-content-conclusion_description">
-                            Investir em um sistema de CFTV de qualidade é uma decisão inteligente para proteger seus ativos e garantir a tranquilidade de sua empresa. Confie na experiência e na excelência da Connect House para fornecer soluções de segurança de classe mundial.
+
+                            Além disso, realizamos a instalação de TVs em pontos estratégicos do ambiente, proporcionando uma experiência visual imersiva. Combinamos a funcionalidade da automação com a qualidade das TVs para oferecer um ambiente completo e moderno.
                         </p>
                     </div>
 
@@ -140,7 +142,11 @@ const LightingProject: React.FC = () => {
                             Contate-nos
                         </h2>
                         <p className="project-content-contact-us_description">
-                            Se você está procurando uma instalação profissional de sistemas de CFTV que atenda às suas necessidades específicas, entre em contato conosco hoje mesmo. Nossa equipe especializada está pronta para discutir suas demandas e fornecer a melhor solução de segurança para o seu negócio.
+                            Se você deseja transformar seu espaço com um projeto de instalação elétrica e iluminação personalizado, entre em contato conosco. Nossa equipe de especialistas terá o prazer de ajudá-lo a criar um ambiente funcional, seguro e visualmente deslumbrante.
+                        </p>
+
+                        <p className="project-content-contact-us_description">
+                            Aproveite para agendar uma consulta gratuita e descubra como nossos serviços podem agregar valor ao seu ambiente. Estamos disponíveis no WhatsApp e Telegram para atender suas dúvidas e oferecer soluções personalizadas.
                         </p>
 
                         <div className="project-content-contact-us_links">
